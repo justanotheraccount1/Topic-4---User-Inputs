75c0a87798c9dd21a7abe7747393efa23dfd7b42
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-13 09:15:11 -0400

    finished

--- Topic 4 - User Inputs/Program.cs
+++ Topic 4 - User Inputs/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             //Sam Ahrens 09/12/24
-            string userName, name, pizzaTopping;
+            string userName, name, pizzaTopping, itemName;
             int age;
-            double price;
+            double price, itemPrice, diameter, trippleItemPrice, radius;
 
             Console.ForegroundColor = ConsoleColor.Green;
 
@@ -30,17 +30,32 @@
             //Console.WriteLine("Your total comes to " + ((price / 5) * 4).ToString("C"));
             //Console.WriteLine();
 
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
-            Console.WriteLine();
+            //Console.WriteLine("What is your name?");
+            //name = Console.ReadLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("Hey " + name + "! What is your favorite pizza topping?");
-            pizzaTopping = Console.ReadLine();
-            Console.WriteLine();
+            //Console.WriteLine("Hey " + name + "! What is your favorite pizza topping?");
+            //pizzaTopping = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("I gotta admit " + name + ", " + pizzaTopping + " is a fire pizza topping!");
+            //Console.WriteLine();
 
-            Console.WriteLine("I gotta admit " + name + ", " + pizzaTopping + " is a fire pizza topping!");
+            //Console.WriteLine("What is the name of the item?");
+            //itemName = Console.ReadLine();
+            //Console.WriteLine("What is the price of the item?");
+            //Console.Write("$");
+            //Double.TryParse(Console.ReadLine(), out itemPrice);
+            //trippleItemPrice = itemPrice * 3;
+            //Console.WriteLine("The price of 3 " + itemName + "s would be $" + trippleItemPrice);
+            //Console.WriteLine();
+
+            Console.WriteLine("What is the diameter of the circle in centimeters?");
+            Double.TryParse(Console.ReadLine(), out diameter);
+            radius = diameter / 2;
+            Console.WriteLine("The radius of the circle is " + radius + "cm");
 
 
+            
         }
     }
 }
